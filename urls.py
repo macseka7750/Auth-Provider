657ab44c131f5cb53c4a9380355d57c4e78a52e2
macseka <matrixhunter911@gmail.com>
2026-04-28 13:40:16 +0300

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import PostListCreateView, PostDetailView
+
+urlpatterns = [
+    path('', PostListCreateView.as_view(), name='post-list'),
+    path('<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
+]
